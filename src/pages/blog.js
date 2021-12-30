54e26dd3d76ba08ae8f88e79fa84e418f77079a2
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-12-30 18:07:12 +0200

    v5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({ data }) => {
 
@@ -9,14 +10,16 @@ const BlogPage = ({ data }) => {
         <Layout pageTitle="My Blog Posts">
             <ul>
                 {
-                    data.allFile.nodes.map(node => {
-                        if (node.sourceInstanceName === 'blog') return (
-                            <li key={node.name}>
-                                {node.name}
-                            </li>
-                        )
-                        return null
-                    })
+                    data.allMdx.nodes.map(node => (
+                        <li key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </li>
+                    )
+                    )
                 }
             </ul>
         </Layout>
@@ -25,10 +28,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
-        sourceInstanceName
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
