a00b057bc67721bfae2c9b71e9365a5ea8b16beb
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-12-31 13:49:11 +0200

    idk working

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,44 +1,45 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../components/layout'
 
-const BlogPage = ({ data }) => {
+const BlogPage = () => {
 
-    console.log(data);
-    return (
-        <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allMdx.nodes.map(node => (
-                        <li key={node.id}>
-                            <h2>{node.frontmatter.title}</h2>
-                            <p>Posted: {node.frontmatter.date}</p>
-                            <MDXRenderer>
-                                {node.body}
-                            </MDXRenderer>
-                        </li>
-                    )
-                    )
-                }
-            </ul>
-        </Layout>
-    )
+  const data = useStaticQuery(
+    graphql`
+query MyQuery {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+      body
+      id
+    }
+
+  }
 }
 
-export const query = graphql`
-  query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-        }
-        id
-        body
-      }
-    }
-  }
 `
+  )
+
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
+        ))
+      }
+    </Layout>
+  )
+}
+
 
 export default BlogPage
